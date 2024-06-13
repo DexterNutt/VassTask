8074491284587e524cd95be867cf5189ee94cc5a
DexterNutt <demjan.a.golubov@gmail.com>
2024-06-13 13:19:47 +0200

    write out the css and implement the rating component

--- VassTask/src/App.jsx
+++ VassTask/src/App.jsx
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
-
-import "./App.module.css";
+import { RatingReview } from "./starsComp/StarsRating";
+import styles from "./App.module.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [rating, setRating] = useState(0);
 
   return (
     <>
-      <div className="container"></div>
+      <div className={styles.componentContainer}>
+        <div className={styles.title}>
+          <h4>Please Rate</h4>
+        </div>
+        <div className={styles.starsContainer}>
+          <RatingReview rating={rating} setRating={setRating} />
+        </div>
+        <div className={styles.text}>
+          <p>Your feedback is very important to us.</p>
+        </div>
+        <img src="/logo.png" alt="Vass logo" />
+      </div>
     </>
   );
 }
