1c65b595dd9069c12ec77a4c54edf62bf3e06417
DexterNutt <demjan.a.golubov@gmail.com>
2024-06-13 14:48:03 +0200

    implement local storage for ratings

--- VassTask/src/App.jsx
+++ VassTask/src/App.jsx
@@ -1,8 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./App.module.css";
 
+const storedData = localStorage.getItem("rating");
+
 function App() {
-  const [rating, setRating] = useState(null);
+  const [rating, setRating] = useState([]);
+  useEffect(() => {
+    setRating(storedData);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("rating", JSON.stringify(rating));
+  }, [rating]);
 
   return (
     <>
@@ -11,9 +20,10 @@ function App() {
           <h4>Please Rate</h4>
         </div>
         <div className={styles.starsContainer}>
-          {[1, 2, 3, 4, 5].map((star) => {
+          {[1, 2, 3, 4, 5].map((star, i) => {
             return (
               <img
+                key={i}
                 className={styles.star}
                 src={rating >= star ? "/ShapeGold.png" : "/Shape.png"}
                 onClick={() => {
@@ -24,7 +34,10 @@ function App() {
           })}
         </div>
         <div className={styles.text}>
-          <p>Your feedback is very important to us.</p>
+          <p>
+            Your Feedback Is Very
+            <br /> Important To Us.
+          </p>
         </div>
         <img src="/logo.png" alt="Vass logo" />
       </div>
